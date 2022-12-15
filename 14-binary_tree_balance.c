96934769113e7a5ed4a71faf53d075d981212f60
Mumin8 <alhassanmumin8@gmail.com>
2022-12-15 14:28:33 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,6 +14,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		l = tree->left ? 1 + binary_tree_balance(tree->left): 0;
 		r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+		return (l - r);
 	}
-	return (l - r);
+	return (0);
 }

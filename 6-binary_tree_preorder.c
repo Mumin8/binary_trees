1b31c97cfd2e8f6f03e6425b6732370475ec897c
Mumin8 <alhassanmumin8@gmail.com>
2022-12-15 12:03:20 -0800

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_preorder - the function to print preorder
+ *
+ * @tree: the tree to print
+ * @func: the function pointer
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)

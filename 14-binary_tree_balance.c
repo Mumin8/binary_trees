3ca4d61870bc52e160634e7a6f916a98a36df99f
Mumin8 <alhassanmumin8@gmail.com>
2022-12-15 14:18:32 -0800

    balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - balanced tree
+ *
+ * @tree: the tee
+ * Return: the difference
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (tree)
+	{
+		l = tree->left ? 1 + binary_tree_balance(tree->left): 0;
+		r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	}
+	return (l - r);
+}
